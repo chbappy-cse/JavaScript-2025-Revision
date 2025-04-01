dae90bbced784b5ecb349df0d1c969ea0759222e
AP-BAPPI-WEBAPP\bappi.chandra <bappi.chandra@amiprobashi.com>
2025-04-01 10:40:23 +0600

    Includes, IndexOf and LastIndexOf

---
+++ JavaScript Array/43 - Includes, IndexOf and LastIndexOf.js
@@ -0,0 +1,15 @@
+/**
+ * Includes, IndexOf and LastIndexOf
+ */
+
+const arr = ['One', 'Two', 'Three'];
+
+// Includes
+console.log(arr.includes('One'));
+
+// IndexOf
+console.log(arr.indexOf('Two'));
+
+// LastIndexOf
+console.log(arr.lastIndexOf('Two'));
+
